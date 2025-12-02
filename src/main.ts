01b3fecbe9d791a9d31f5cb3931b69f9a4a15262
mehdi-larek <larek.med@gmail.com>
2025-12-02 09:29:02 +0100

    core: remplacement de bootstrap par tailwind

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
 
 createApp(App)
     .use(router)
