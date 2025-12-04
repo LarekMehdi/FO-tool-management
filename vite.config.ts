fa4bfe0f2958136a2d35c1974a761676c7b0a3b2
mehdi-larek <larek.med@gmail.com>
2025-12-04 17:03:00 +0100

    core: pouvoir changer le port facilement

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default ({ mode }: { mode: string }) => {
+
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+    },
+  });
+};
